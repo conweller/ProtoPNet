5add2bb1f535f3730a2a887f99b39d8a59758344
Connor Onweller <onweller@udel.edu>
2021-03-17 19:58:12 -0400

    create directories

--- setup.sh
+++ setup.sh
@@ -5,6 +5,17 @@ gdown --id 1hbzc_P1FuxMkcabkgn9ZKinBwW683j45 --output CUB_200_2011.tgz
 
 tar -xf CUB_200_2011.tgz
 
+mkdir datasets
+mkdir datasets/cub200_cropped/
+mkdir datasets/cub200_cropped/test_cropped
+mkdir datasets/cub200_cropped/train_cropped
+
+for dir in $(ls CUB_200_2011/images); do
+    mkdir datasets/cub200_cropped/test_cropped/$dir
+    mkdir datasets/cub200_cropped/train_cropped/$dir
+done
+
+
 python3 crop.py
 
 python3 img_aug.py
