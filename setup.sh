cc4d5ef98dc9f60968bc3188d4bc00fb54f99635
Connor Onweller <onweller@udel.edu>
2021-04-01 12:06:49 -0400

    Added messages to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Downloading Base Dataset"
+
 gdown --id 1hbzc_P1FuxMkcabkgn9ZKinBwW683j45 --output CUB_200_2011.tgz
 
 tar -xf CUB_200_2011.tgz
@@ -15,6 +17,10 @@ for dir in $(ls CUB_200_2011/images); do
 done
 
 
+echo "Cropping Images"
+
 python3 crop.py
+
+echo "Constructing Augmented Images"
 
 python3 img_aug.py
